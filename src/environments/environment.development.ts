d97de262db357e279083eb32bae7466c9c260494
Karen Mirzayan <k_mirzayan@kbtu.kz>
2024-11-17 16:55:44 +0500

    Envs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   contractAddress: '0x07394919b9a3561560e1B45bCd4A628eED072596',
   bnbTestnet: 'https://data-seed-prebsc-1-s1.binance.org:8545/' // BNB Testnet RPC URL
 };
